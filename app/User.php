a7d6e74130ee5ece2f69320d90d8cf5a9cfd160c
biawal <bilawal.jabbar.786@gmail.com>
2020-10-23 17:24:39 +0500

    New code auth setup

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'firstname','lastname', 'email', 'password', 'phone', 'city', 'country', 'role'
+        'firstname','lastname', 'email', 'password', 'phone', 'city', 'country', 'role', 'address'
     ];
 
     /**
