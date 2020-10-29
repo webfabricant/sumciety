b00e912562e109377a0fdea8f86350d4876dc19a
M Afzal <muhammadafzalibrahim@gmail.com>
2020-10-29 23:09:48 +0500

    title

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -6,7 +6,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>SocialV - Responsive Bootstrap 4 Admin Dashboard Template</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Favicon -->
     <link rel="shortcut icon" href="images/favicon.ico" />
     <!-- Bootstrap CSS -->
